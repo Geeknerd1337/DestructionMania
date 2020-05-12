39d869e3629193eebd191eaac64008857223e9b7
Stephen Hilliard <istepable@tutanota.com>
2020-05-11 23:08:55 -0500

    Adding in stuff.
    The vscode is for me since I use Visual Studio code. I'm using the Brackeys tutroritals for the gun, just in case you want to show a gun while we shoot.

---
+++ Destruction/Assets/Scripts/UI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+  public Text text;
+  public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      text.text = player.transform.position.z.ToString();
+    }
+}
