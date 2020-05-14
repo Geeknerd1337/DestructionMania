b0f740282581e022622cb0857cd599802c4eb07d
Geeknerd1337 <geeknerd1337@gmail.com>
2020-05-14 00:14:20 -0500

    PreShaderForge

---
+++ Destruction/Assets/My assets/Scripts/Player/Weapons/Sledge.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sledge : Weapon
+{
+    private Animator animator;
+    public bool swinging;
+    private AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !animator.GetCurrentAnimatorStateInfo(0).IsName("SledgeSwing"))
+        {
+            Swing();
+            
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("SledgeSwing") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.1f)
+        {
+            
+            swinging = true;
+        }
+        else
+        {
+            swinging = false;
+        }
+    }
+
+    void Swing()
+    {
+        animator.Play("SledgePrepare");
+    }
+
+    public void PlaySound()
+    {
+        sound.Play();
+    }
+}
---
+++ Destruction/Assets/My assets/Scripts/Player/WorldWeapons/SledgeHead.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SledgeHead : MonoBehaviour
+{
+    public Sledge sledge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hello");
+        if (sledge.swinging)
+        {
+
+            Shatter shatter = other.gameObject.GetComponent<Shatter>();
+            if (shatter != null)
+            {
+                shatter.Obliviate();
+            }
+            else
+            {
+                other.attachedRigidbody.AddForce(500f * transform.forward);
+            }
+            
+
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (sledge.swinging)
+        {
+
+            Shatter shatter = other.gameObject.GetComponent<Shatter>();
+            if (shatter != null)
+            {
+                shatter.Obliviate();
+            }
+            else
+            {
+                if (other.attachedRigidbody != null)
+                {
+                    other.attachedRigidbody.AddForce(100f * transform.forward);
+                }
+            }
+
+
+        }
+    }
+}
