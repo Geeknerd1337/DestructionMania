bbd7df2d124b8a4493e950b4f3032342b5b32218
Geeknerd1337 <geeknerd1337@gmail.com>
2020-05-20 11:58:23 -0500

    New Weapon

---
+++ Destruction/Assets/My assets/Scripts/Player/Weapons/RebarLauncher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RebarLauncher : Weapon
+{
+
+    public Animator a;
+    public ParticleSystem spinpart;
+    public ParticleSystem collectPart;
+    public Transform projectilePoint;
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !a.GetCurrentAnimatorStateInfo(0).IsName("Fire"))
+        {
+            Shoot();
+
+        }
+    }
+
+    void Shoot()
+    {
+        a.Play("Fire");
+        spinpart.Play();
+        collectPart.Play();
+        GetComponent<AudioSource>().Play();
+    }
+
+    void FireProjectile()
+    {
+        GameObject g = Instantiate(projectile);
+        g.transform.position = projectilePoint.position;
+        g.transform.rotation = projectilePoint.rotation;
+        g.GetComponent<Rigidbody>().AddForce(g.transform.forward * 4000f);
+       
+    }
+}
---
+++ Destruction/Assets/My assets/Scripts/Player/WorldWeapons/RebarProjectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RebarProjectile : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float time = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time < 0)
+        {
+            rb.useGravity = true;
+        }
+    }
+}
