646f96ff460f867c4512255e8a444a2273332043
Stephen Hilliard <istepable@tutanota.com>
2020-05-28 20:19:44 -0500

    Added Backgrounds to Menus

---
+++ Destruction/Assets/Stephen assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+   public GameObject Obj;
+
+   public float x,y,z;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Obj.transform.Rotate(x * Time.deltaTime,y * Time.deltaTime,z * Time.deltaTime,Space.World);
+    }
+}
